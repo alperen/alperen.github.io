6995ff0f64e234f3e7b197549d7efb693187dfb5
Alperen Turkoz <alperentrkz@gmail.com>
2019-01-09 15:14:38 +0300

    gulpfile is ready for scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
-const gulp = require('gulp');
-const webpack = require('webpack-stream');
-var sass = require('gulp-sass');
+const gulp = require ('gulp');
+const sass = require ('gulp-sass');
+const through = require ('through2');
+const replace = require ('gulp-replace');
 
+gulp.task ('sass', () => {
+  gulp
+    .src (['./scss/main.scss'])
+    .pipe (replace ('@import "~/', '@import "./node_modules/'))
+    .pipe (
+      sass ({
+        outputStyle: 'compressed',
+        precision: 10,
+      }).on ('error', sass.logError)
+    )
+    .pipe (gulp.dest ('./public/main2bundle.css'));
+});
 
+gulp.task ('sass:watch', () => {
+  gulp.watch (['./scss/**/**.scss'], ['sass']);
+});
 
-gulp.task('default',['webpack:watch']);
+gulp.task ('default', ['sass:watch']);
