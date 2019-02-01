e4865539bc6dc354d91b18db0d43a45207b897f9
Alperen Turkoz <alperentrkz@gmail.com>
2019-02-01 22:17:42 +0300

    firebase context deleted

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export default class Footer extends React.Component {
-  constructor () {
-    super ();
+  constructor() {
+    super();
     //this.darkSideOfTheMoon = this.darkSideOfTheMoon.bind(this);
   }
 
-  render () {
+  render() {
     return (
-      <div className="d-flex align-items-end" style={{height: 100}}>
+      <div className="d-flex align-items-end" style={{ height: 100 }}>
         <div className="d-flex align-items-center">
           <div>
             <div className="dark-side-off-the-moon" />
@@ -17,17 +17,7 @@ export default class Footer extends React.Component {
             <span> Thank you for your interest. </span> <br />
             <span className="text-muted text-small">
               {' '}Last Update on Content: 04.01.2019
-              <span
-                data-balloon="This website is serving on Google Firebase"
-                data-balloon-pos="down"
-              >
-                {' '}
-                <img
-                  src="./img/firebase.png"
-                  alt="This website is serving on Google Firebase"
-                />
-                {' '}
-              </span>
+
             </span>
           </div>
         </div>
