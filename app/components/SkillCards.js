aa6ce6ceb17c94021c51c665fd6ad262172c5084
Alperen Turkoz <alperentrkz@gmail.com>
2019-02-01 22:17:54 +0300

    content update

--- app/components/SkillCards.js
+++ app/components/SkillCards.js
@@ -4,55 +4,56 @@ import posed from 'react-pose';
 import analytics from '../analytics';
 
 const colors = [
-  {color: '#F7231A', name: 'Beginner'},
-  {color: '#FAA219', name: 'Medium'},
-  {color: '#4BCF9B', name: 'Enough'},
+  { color: '#F7231A', name: 'Beginner' },
+  { color: '#FAA219', name: 'Medium' },
+  { color: '#4BCF9B', name: 'Enough' },
+  { color: '#523973', name: 'Interested-In' },
 ];
 
 export default class SkillCards extends React.Component {
-  constructor () {
-    super ();
-    this.filterBySkillLevel = this.filterBySkillLevel.bind (this);
-    this.filterBySkillLevelRemove = this.filterBySkillLevelRemove.bind (this);
+  constructor() {
+    super();
+    this.filterBySkillLevel = this.filterBySkillLevel.bind(this);
+    this.filterBySkillLevelRemove = this.filterBySkillLevelRemove.bind(this);
   }
 
-  filterBySkillLevel (id) {
-    document.querySelectorAll (`.c-skill-single:not(.L-${id})`).forEach (e => {
-      e.classList.add ('opacitied');
+  filterBySkillLevel(id) {
+    document.querySelectorAll(`.c-skill-single:not(.L-${id})`).forEach(e => {
+      e.classList.add('opacitied');
     });
 
-    analytics.event ({
+    analytics.event({
       category: 'SKILLS',
       action: 'SKILLS_FILTERED',
       label: id,
     });
   }
 
-  filterBySkillLevelRemove (id) {
-    document.querySelectorAll (`.c-skill-single:not(.L-${id})`).forEach (e => {
-      e.classList.remove ('opacitied');
+  filterBySkillLevelRemove(id) {
+    document.querySelectorAll(`.c-skill-single:not(.L-${id})`).forEach(e => {
+      e.classList.remove('opacitied');
     });
   }
 
-  render () {
+  render() {
     return (
       <div className="c-skill-levels opacity-on-drag">
-        {colors.map (({color, name}, ii) => {
+        {colors.map(({ color, name }, ii) => {
           return (
             <div
               key={ii}
               className="label"
               id={name}
               onMouseOver={() => {
-                this.filterBySkillLevel (name);
+                this.filterBySkillLevel(name);
               }}
               onMouseLeave={() => {
-                this.filterBySkillLevelRemove (name);
+                this.filterBySkillLevelRemove(name);
               }}
             >
               <span>
                 {' '}
-                <span className="level-box" style={{backgroundColor: color}} />
+                <span className="level-box" style={{ backgroundColor: color }} />
                 {' '}
                 <span className="s pl-2">{name}</span>
                 {' '}
--- app/components/Skills.js
+++ app/components/Skills.js
@@ -4,37 +4,40 @@ const colors = {
   red: '#F7231A',
   yellow: '#FAA219',
   green: '#4BCF9B',
+  purple: '#523973',
 };
 
 const SkillsList = [
-  {text: 'Javascript', color: colors.green},
-  {text: 'React', color: colors.green},
-  {text: 'React Native', color: colors.yellow},
-  {text: 'Next.js', color: colors.red},
-  {text: 'PHP', color: colors.green},
-  {text: 'Laravel', color: colors.yellow},
-  {text: 'MYSQL', color: colors.green},
-  {text: 'Firebase', color: colors.yellow},
-  {text: 'SCSS', color: colors.yellow},
-  {text: 'webpack', color: colors.red},
-  {text: 'Bootstrap', color: colors.green},
-  {text: 'jest', color: colors.red},
+  { text: 'Javascript', color: colors.green },
+  { text: 'React', color: colors.green },
+  { text: 'React Native', color: colors.yellow },
+  { text: 'Next.js', color: colors.red },
+  { text: 'PHP', color: colors.green },
+  { text: 'Laravel', color: colors.yellow },
+  { text: 'MYSQL', color: colors.green },
+  { text: 'Firebase', color: colors.yellow },
+  { text: 'SCSS', color: colors.yellow },
+  { text: 'webpack', color: colors.red },
+  { text: 'Bootstrap', color: colors.green },
+  { text: 'jest', color: colors.red },
+  { text: 'Go', color: colors.purple },
+  { text: 'Docker', color: colors.purple },
 ];
 
 const getColorLabel = color => {
   return color == colors.red
     ? 'L-Beginner'
-    : color == colors.green ? 'L-Enough' : 'L-Medium';
+    : color == colors.green ? 'L-Enough' : color == colors.purple ? 'L-Interested-In' : 'L-Medium';
 };
 
 export default class Skills extends React.Component {
-  render () {
+  render() {
     return (
       <div className="row">
-        {SkillsList.map (({text, color}, index) => {
+        {SkillsList.map(({ text, color }, index) => {
           return (
             <div
-              className={`col col-6 mb-2 c-skill-single ${getColorLabel (color)}`}
+              className={`col col-6 mb-2 c-skill-single ${getColorLabel(color) || ''}`}
               key={index}
             >
               <span>
@@ -42,7 +45,7 @@ export default class Skills extends React.Component {
                 {' '}
                 {text}
                 {' '}
-                <span className="level" style={{backgroundColor: color}} />
+                <span className="level" style={{ backgroundColor: color }} />
               </span>
             </div>
           );
