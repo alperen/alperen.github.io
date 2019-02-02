9544c93c31e3a32e21f04c1ef55d8ac16993a7f4
Alperen Turkoz <alperentrkz@gmail.com>
2019-02-03 00:12:25 +0300

    eslint final

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-    "extends": "airbnb/base"
+    "extends": "airbnb",
+    "rules": {
+        "react/prefer-stateless-function": "off",
+        "react/no-array-index-key": "off",
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    }
 };
