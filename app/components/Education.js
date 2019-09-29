3d223d93f2595925f413cadda6b4aee747f31076
Alperen Türköz <alperentrkz@gmail.com>
2019-09-29 21:33:14 +0300

    feat(education): revision education section logics

--- app/components/Education.js
+++ app/components/Education.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-const list = [
-    {title:'İTO Vakfı Süleyman Taştekin Techincal High School',content:'Between 2012 and 2016. Web Design and Programming Department'},
-    {title:'Samsung Akademi İzmir', content:'I graduated as firstly. On depertmant of Computer'},
-    {title:'Mugla Sitki Kocman Universitesi', content:'I\'ve joined 2017 September to university. Now i\'m studying second grade IT Engineering at Technology Depertmant'}
-];
+import {educationHistory} from '../constants/educationConstants';
 
 function Education() {
   return (
     <div>
-      {list.slice(0).reverse().map(({title,content = null}) => {
-          return <details key={Math.random()}>
-          <summary>{title}</summary>
-          <p className="text-black-50 text-small">{content}</p>
-        </details>
+      {educationHistory.reverse().map(({title,content = null}) => {
+          return (
+            <details key={Math.random()}>
+              <summary>{title}</summary>
+
+              <p className="text-black-50 text-small">{content}</p>
+            </details>
+          );
       })}
     </div>
   )
