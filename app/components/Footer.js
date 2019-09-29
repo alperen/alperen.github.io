5635cb229f2fa9f3f5bad99b8dfaacc50f60f4b2
Alperen Türköz <alperentrkz@gmail.com>
2019-09-29 21:38:07 +0300

    feat(footer): change into stateless component

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export default class Footer extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div className="d-flex align-items-end" style={{ height: 100 }}>
-        <div className="d-flex align-items-center">
-          <div className="pl-2">
-            <span> Thank you for your interest. </span> <br />
-            <span className="text-muted text-small">
-              {' '}Last Update on Content: 04.01.2019
-            </span>
-          </div>
+function Footer() {
+  return (
+    <div className="d-flex align-items-end" style={{ height: 100 }}>
+      <div className="d-flex align-items-center">
+        <div className="pl-2">
+          <span> Thank you for your interest. </span> <br />
+          <span className="text-muted text-small">
+            Last Update on Content: 04.01.2019
+          </span>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Footer;
